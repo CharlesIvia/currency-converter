63c1204eb8c4ea57543265a4212fd7b423b570b9
Charles Ivia <iviacharles@gmail.com>
2020-07-21 14:05:07 +0300

    Fetched conversion metrics from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import CurrencyRow from "./components/CurrencyRow";
 
+const BASE_URL = "https://api.exchangeratesapi.io/latest";
+
 function App() {
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div className="App">
       <h1>Currency Converter</h1>
