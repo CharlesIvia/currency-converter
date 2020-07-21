323518b8f172199cc33611e1699c27ef2e718670
Charles Ivia <iviacharles@gmail.com>
2020-07-21 13:32:38 +0300

    Created and added CurrencyRow component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import CurrencyRow from "./components/CurrencyRow";
 
 function App() {
-  return <div className="App">
-    <h1>Hello World!</h1>
-  </div>;
+  return (
+    <div className="App">
+      <CurrencyRow />
+      <div>=</div>
+      <CurrencyRow />
+    </div>
+  );
 }
 
 export default App;
